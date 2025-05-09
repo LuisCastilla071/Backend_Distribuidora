25abc9837363f721dd41c774c0bc603b4aa7bc17
LuisCastilla071 <luisjaviercastillasuare@gmail.com>
2025-05-09 11:24:52 -0600

    Se sube el backend

--- src/routes/ventas.routes.js
+++ src/routes/ventas.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { obtenerVentasConDetalles, obtenerVentas, eliminarVenta, registrarVenta,actualizarVenta} from '../controllers/ventas.controller.js';
+import { obtenerVentasConDetalles, obtenerVentas, eliminarVenta, registrarVenta,actualizarVenta,obtenerVentaPorId} from '../controllers/ventas.controller.js';
 
 const router = Router();
 
@@ -17,5 +17,8 @@ router.post('/registrarventa', registrarVenta);
 
 // Ruta para actualizar una venta
 router.patch('/actualizarventa/:id_venta', actualizarVenta);
+
+// Ruta para obtener venta por id
+router.get('/obtenerventaporid/:id_venta', obtenerVentaPorId);
 
 export default router;
