9e5de27dee226f26ce9dea464f51759d549e6dd2
LuisCastilla071 <luisjaviercastillasuare@gmail.com>
2025-05-28 16:56:45 -0600

    Se suben los archivos

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import rutasDetallesVentas from './routes/detalles_ventas.routes.js';
 import rutasDetallesCompras from'./routes/detalles_compras.routes.js';
 import rutasCompras from './routes/compras.routes.js';
 import rutasEstadisticas from './routes/estadisticas.routes.js';
+import rutasIA from './routes/ia.routes.js';
 const app = express();
 
 // Habilitar CORS para cualquier origen
@@ -33,6 +34,7 @@ app.use('/api', rutasDetallesVentas);
 app.use('/api', rutasDetallesCompras);
 app.use('/api', rutasCompras);
 app.use('/api', rutasEstadisticas);
+app.use('/ia', rutasIA);
 
 // Manejo de rutas no encontradas
 app.use((req, res, next) => {
